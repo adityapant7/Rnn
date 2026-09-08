d58c98fece38555b31445076a4dea742f36b8737
adityapant7 <pantaditya37@gmail.com>
2026-09-08 20:15:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, SimpleRNN
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.callbacks import EarlyStopping
+vac_size=1000
+#loading imbd data
+(X_train,Y_train),(X_test,Y_test)=imdb.load_data(num_words=vac_size)
+sent_len=500
+#padding making each sentance of eual length
+X_train = sequence.pad_sequences(X_train, maxlen=sent_len)
+X_test=sequence.pad_sequences(X_test,maxlen=sent_len)
+dim=128
+#creating an simple rnn model
+model=Sequential()
+model.add(Embedding(vac_size,dim,input_length=sent_len))
+model.add(SimpleRNN(128,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+earlystopping=EarlyStopping(monitor='val_loss',patience=5,restore_best_weights=True)
+#train the model with early stopping
+history=model.fit(X_train,Y_train,epochs=10,callbacks=earlystopping,validation_split=0.2)
+model.save('model.keras')
---
+++ prediction.py
@@ -0,0 +1,35 @@
+import tensorflow as tf 
+from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+import streamlit as st
+model=load_model('model.keras')
+word_index=imdb.get_word_index()
+reverse_index={value:key for key,value in word_index.items()}
+# function to decode
+def decode(encode):
+    return ' '.join([reverse_index.get(i-3,'?') for i in encode])
+#preprocessing of new input
+def preprocess_text(text):
+    words=text.lower().split()
+    encode=[word_index.get(i,2)+3 for i in words]
+    pad_review=sequence.pad_sequences([encode],maxlen=500)
+    return pad_review
+def predict_sentiment(review):
+    predict_sentiment_output=preprocess_text(review)
+    prediction=model.predict(predict_sentiment_output)
+    sentiment='Positive' if prediction[0][0]>0.5 else 'Negative'
+    return sentiment,prediction[0][0]
+#streamlit app
+st.title("IMDB SENTIMENT ANALYSIS")
+st.write("Enter a movie review to check if it is positive or negative ")
+user_review=st.text_area("enter your review")
+if st.button('classification'):
+    sentiment,score=predict_sentiment(user_review)
+    st.write("sentiment=",sentiment)
+    st.write("prediction score=",score)
+else:
+    st.write("enter review")
+
+
